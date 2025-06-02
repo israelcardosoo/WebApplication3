8dc88156547012ac3717fa1815b1b04290e987ea
israelcardoso <israel.santos105@etec.sp.gov.br>
2025-06-02 09:06:53 -0300

    Alteração

--- WebApplication3/Models/Produtos.cs
+++ WebApplication3/Models/Produtos.cs
@@ -1,4 +1,6 @@
-﻿namespace WebApiFinal.Models
+﻿using WebApiFinal.Migrations;
+
+namespace WebApiFinal.Models
 {
     public class Produtos : Base
     {
@@ -6,6 +8,21 @@
         public string? Descricao { get; set; }
         public int Quantidade { get; set; }
         public decimal Valor { get; set; }
+
+
+
+        //Tipos de Dados
+
+        string nome = "Etec"; //Tipo de referencia
+        int valor = 10; //Números inteiros
+        double valor2 = 20; //Numeros interios
+
+        long valorF = 10.5F; //Numeros reais
+        byte valor 4 = 10; //Numeros reais
+
+
+
+
 
     }
 }
